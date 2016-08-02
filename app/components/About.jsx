c29da1251207d603df8f0a464281d7c9892f9c85
Rubèn Nieto <ruben.np@me.com>
2016-08-02 21:05:42 +0200

    Styled About Component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,24 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>About me page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        Welcome! My name is Rubèn and this is my first web app using React!
+        Is a weather application for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+      <hr></hr>
+      <p className="text-center"><strong>Feel free to search for the weather in your city! See you soon!</strong></p>
     </div>
   );
 };
