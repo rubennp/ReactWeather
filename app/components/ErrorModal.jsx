c886aeba3467f0bff743cd3560ba12bbaed0fa97
Rubèn Nieto <ruben.np@me.com>
2016-08-02 22:09:26 +0200

    Added ErrorModal.jsx

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var ErrorModal = React.createClass({
+  componentDidMount: function () {
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {
+    return (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>Some Title</h4>
+        <p>Our error message!</p>
+        <button type="button" className="button hollow" data-close="">Okay</button>
+      </div>
+    );
+  }
+});
+
+module.exports = ErrorModal;
