08f951facac523070897f578631fba896a857f7b
Rubèn Nieto <ruben.np@me.com>
2016-08-02 22:08:45 +0200

    Added ErrorModal (todo: close button not working)

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,19 +1,22 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
-var openWeatherMap = require('openWeatherMap')
+var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-      isLoading: false,
-      errorMessage: ''
+      isLoading: false
     }
   },
   handleSearch: function (location) {
     var that = this;
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
@@ -21,17 +24,17 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false,
       });
-    }, function (errorMessage) {
+    }, function (e) {
       that.setState({
         location: null,
         temp: null,
         isLoading: false,
+        errorMessage: e.message
       });
-      alert(errorMessage);
     });
   },
   render: function () {
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
     // var location = this.state.location;
     // var temp = this.state.temp;
 
@@ -42,11 +45,21 @@ var Weather = React.createClass({
         return <WeatherMessage location={location} temp={temp}/>;
       }
     }
+
+    function renderError () {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal />
+        );
+      }
+    }
+
     return (
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
