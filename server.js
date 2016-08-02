a608e4d223019d10e1abfed2eeab07e72d1634d3
Rubèn Nieto <ruben.np@me.com>
2016-08-02 15:32:08 +0200

    Copy

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwaded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
+if (PORT !== 3000) {
+  app.use(function (req, res, next) {
+    if (req.headers['x-forwaded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect('http://' + req.hostname + req.url);
+    }
+  });
+}
 
 app.use(express.static('public'));
 
